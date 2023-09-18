fc066f7d9346c18292f6c8121340f90794a8aa10
thpchandrika <thpchandrika@gmail.com>
2023-09-18 01:27:00 -0500

    ui update

--- src/main/java/edu/mum/cs/cs425/demowebapps/elibrary/controller/HomeController.java
+++ src/main/java/edu/mum/cs/cs425/demowebapps/elibrary/controller/HomeController.java
@@ -2,11 +2,12 @@ package edu.mum.cs.cs425.demowebapps.elibrary.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-	 @GetMapping(value = {"/", "/eregistrar", "/eregistrar/home"})
+
+	 @GetMapping(value = {"/", "/eregistrar","/eregistrar/home"})
 	  public String displayHomePage() {
 		  return "home/index";
 	  }
--- src/main/java/edu/mum/cs/cs425/demowebapps/elibrary/controller/StudentController.java
+++ src/main/java/edu/mum/cs/cs425/demowebapps/elibrary/controller/StudentController.java
@@ -2,12 +2,19 @@ package edu.mum.cs.cs425.demowebapps.elibrary.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = {"/eregistrar/students"})
 public class StudentController {
 
-    @GetMapping(value = {"/eregistrar/students","/eregistrar/student/home"})
-    public String displayHomePage() {
+    @GetMapping()
+    public String displayStudentList() {
         return "student/index";
+    }
+
+    @GetMapping("/register")
+    public String registerNewStudent() {
+        return "student/register";
     }
 }
